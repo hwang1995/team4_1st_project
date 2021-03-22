bd401398e4af8db3f77a278b8417dc06b231abba
SUNG WOOK HWANG <destiny0810@naver.com>
2021-03-22 19:21:40 +0900

    Add MyBatis and JDBC

--- src/main/java/com/team4/webapp/controller/AccountController.java
+++ src/main/java/com/team4/webapp/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.team4.webapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/account")
 public class AccountController {
+	// 로거 설정
+	private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 	
 	@GetMapping("/mypage")
 	public String myPage() {
