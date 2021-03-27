25255486661734787f99a14dd0381486e94e4ba7
ParkVitna <bbn960903@gmail.com>
2021-03-27 15:16:21 +0900

    Category-DAO/ ProductImg-DAO import 수정

--- src/main/java/com/team4/webapp/dao/CategoriesDAO.java
+++ src/main/java/com/team4/webapp/dao/CategoriesDAO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.team4.webapp.dto.CategoriesDTO;
-import com.team4.webapp.dto.ColorsDTO;
 
 @Mapper
 public interface CategoriesDAO {
