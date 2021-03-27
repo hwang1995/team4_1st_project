57b5fcaa94b4d91ecf3c975e4ee77212467d5637
psm7047 <psm7047@naver.com>
2021-03-27 15:04:32 +0900

    carts.xml, CartsDAO.java, orders.xml, OrdersDAO.java, products.xml, ProductsDAO.java 변경

--- src/main/java/com/team4/webapp/dao/CartsDAO.java
+++ src/main/java/com/team4/webapp/dao/CartsDAO.java
@@ -1,8 +1,17 @@
 package com.team4.webapp.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.team4.webapp.dto.CartsDTO;
 
 @Mapper
 public interface CartsDAO {
-
+	public List<CartsDTO> selectCartsList();
+	public CartsDTO selectByCartId(long cart_id);
+	public int insertCarts(CartsDTO carts);
+	public int updateCarts(CartsDTO carts);
+	public int deleteCarts(long cart_id);
+	
 }
