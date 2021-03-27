e9d1f4bfde3aeb713a21c1dd701d356328065399
gangbyeongju <gangbungjoo@naver.com>
2021-03-27 14:57:08 +0900

    0327 Members, Qnas, Orderlists의 Dao, mapper.xml 파일 수정

--- src/main/java/com/team4/webapp/dao/MembersDAO.java
+++ src/main/java/com/team4/webapp/dao/MembersDAO.java
@@ -1,8 +1,16 @@
 package com.team4.webapp.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.team4.webapp.dto.MembersDTO;
 
 @Mapper
 public interface MembersDAO {
-
+	public List<MembersDTO> selectMembersList();
+	public int insertMembers(MembersDTO membersdto);
+	public MembersDTO selectByMemberId(Long member_id);
+	public int updateMembers(MembersDTO membersdto);
+	public int deleteByMemberId(Long member_id);
 }
--- src/main/java/com/team4/webapp/dao/OrderlistsDAO.java
+++ src/main/java/com/team4/webapp/dao/OrderlistsDAO.java
@@ -1,8 +1,16 @@
 package com.team4.webapp.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.team4.webapp.dto.OrderlistsDTO;
 
 @Mapper
 public interface OrderlistsDAO {
-
+	public List<OrderlistsDTO> selectOrderlistsList();
+	public int insertOrderlists(OrderlistsDTO orderlistsdto);
+	public OrderlistsDTO selectByOrderlistId(Long orderlist_id);
+	public int updateOrderlists(OrderlistsDTO orderlistsdto);
+	public int deleteByOrderlistId(Long orderlist_id);
 }
