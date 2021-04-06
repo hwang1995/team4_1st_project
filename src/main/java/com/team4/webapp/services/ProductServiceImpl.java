68a751667d69c9cbd711a6ff486daff1b9f40384
psm7047 <psm7047@naver.com>
2021-04-06 17:39:33 +0900

    Product 리팩토링 완료

--- src/main/java/com/team4/webapp/services/ProductServiceImpl.java
+++ src/main/java/com/team4/webapp/services/ProductServiceImpl.java
@@ -1,12 +1,8 @@
 package com.team4.webapp.services;
 
 import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.google.common.collect.Lists;
 import com.team4.webapp.dao.ColorsDAO;
 import com.team4.webapp.dao.ProductImgsDAO;
@@ -24,12 +20,6 @@ import com.team4.webapp.dto.SubCategoriesDTO;
 @Service
 public class ProductServiceImpl implements IProductService {
 	
-	/**
-	 * 서비스 목적
-	 * - 모든 사용자들이 웹 사이트의 모든 상품을 보기 위해 제공하는 서비스
-	 * - 컨트롤러에게 List<ProductsDTO> 를 전달해야 한다.
-	 */
-	
 	@Autowired
 	private ProductsDAO productDAO;
 	@Autowired
@@ -41,117 +31,103 @@ public class ProductServiceImpl implements IProductService {
 	@Autowired
 	private SizesDAO sizesDAO;
 	
-	private static final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
-	
+	/**
+	 * 서비스 목적
+	 * - 모든 사용자들이 모든 상품을 보기 위해 제공하는 서비스
+	 * - order = "desc", "high", "low"
+	 * - desc : product_id 기준으로 desc 정렬
+	 * - high : product_price 기준으로 desc 정렬
+	 * - low : product_price 기준으로 asc 정렬
+	 */
 	@Override
 	public List<List<ProductsDTO>> showAllProduct(String order) {
-		// 1. - 만약 orderBy = "DESC" 라면?
-		// 1.1 - ProductsDTO 리스트에 ProductsDAO.selectProductList() 실행
-		// 2. - 만약 orderBy = "HIGH" 라면?
-		// 2.1 - ProductsDTO 리스트에 ProductsDAO.selectProdutsListOrderByHighPrice() 실행
-		// 3. - 만약 orderBy = "LOW" 라면?
-		// 3.1 - ProductsDTO 리스트에 ProductsDAO.selectProductsListOrderByLowPrice() 실행
-		// 4. - 받은 List<ProductsDTO> 객체를 Controller로 넘긴다.
 		
-		List<ProductsDTO> lists;
+		List<ProductsDTO> products;
 		
 		if(order.equals("desc")) {
-			lists = productDAO.selectProductsList();
+			products = productDAO.selectProductsList();
 		}
 		else if(order.equals("high")) {
-			lists = productDAO.selectProdutsListOrderByHighPrice();
+			products = productDAO.selectProdutsListOrderByHighPrice();
 		}else{
-			lists = productDAO.selectProductsListOrderByLowPrice();
+			products = productDAO.selectProductsListOrderByLowPrice();
 		}
-		for(ProductsDTO product : lists ) {
+		//상품의 이미지 경로를 설정하기 위한 for문
+		for(ProductsDTO product : products ) {
 			String filePath = "/webapp/image?path="+ product.getProduct_image();
 			product.setProduct_image(filePath);
 		}
 		
-		List<List<ProductsDTO>> subLists = Lists.partition(lists, 4);
+		//한 row당 4개의 상품을 보여주기 위해 => productLists안에 하나의 리스트 당 상품 4개 담긴 리스트(구아바 라이브러리)
+		List<List<ProductsDTO>> productLists = Lists.partition(products, 4);
 		
-		return subLists;
+		return productLists;
 	}
 
 	/**
 	 * 서비스 목적
-	 * - 모든 사용자들이 카테고리의 상품을 보기 위해 제공하는 서비스
-	 * - 컨트롤러에게 List<ProductsDTO> 를 전달해야 한다.
+	 * - 모든 사용자들이 해당 카테고리의 상품을 보기 위해 제공하는 서비스
+	 * - subcategory = "Jacket, Coat, Cardigan", "Knit, Shirt, Tee", "Pants, Skirt"
+	 * - order = "desc", "high", "low"
+	 * - desc : product_id 기준으로 desc 정렬
+	 * - high : product_price 기준으로 desc 정렬
+	 * - low : product_price 기준으로 asc 정렬 
 	 */
 	@Override
-	public List<ProductsDTO> showProductList(String category, String order) {
-		// 1. - ProductsDAO에 selectProductListWithCategory(String category, String orderBy)를 실행하여 저장
-		// 2. 받은 List<ProductsDAO> 객체를 Controller로 넘긴다.
+	public List<ProductsDTO> showProductByCategory(String subCategory, String order) {
 		
-		 logger.info(category);
-		 SubCategoriesDTO subCategoriesDTO = subCategoriesDAO.selectBySubCategoryName(category);
+		 SubCategoriesDTO subCategoriesDTO = subCategoriesDAO.selectBySubCategoryName(subCategory);
 		 Long subCategory_id = subCategoriesDTO.getSubcategory_id();
-		 List<ProductsDTO> lists;
-		 if(order.equals("desc")) {
-			 lists= productDAO.selectBySubCategoryId(subCategory_id);
-		 }else if(order.equals("high")) {
-			 lists=productDAO.selectBySubCategoryIdOrderByHighPrice(subCategory_id);
-		 }else {
-			 lists=productDAO.selectBySubCategoryIdOrderByLowPrice(subCategory_id);
-		 }
+		 List<ProductsDTO> products;
 		 
-		 for(ProductsDTO product : lists ) {
+		 if(order.equals("desc")) {
+			 products= productDAO.selectBySubCategoryId(subCategory_id);
+		 }else if(order.equals("high")) {
+			 products=productDAO.selectBySubCategoryIdOrderByHighPrice(subCategory_id);
+		 }else {
+			 products=productDAO.selectBySubCategoryIdOrderByLowPrice(subCategory_id);
+		 }
+		
+		 //상품의 이미지 경로를 설정하기 위한 for문
+		 for(ProductsDTO product : products ) {
 				String filePath = "/webapp/image?path="+ product.getProduct_image();
 				product.setProduct_image(filePath);
 			}
 		 
-		 return lists;
-	}
-	/**
-	 * productDetailPage
-	 */
-	@Override
-	public ProductDetailsDTO productDetailPage(Long product_id) {
-		
-		ProductDetailsDTO productDetailsDTO = new ProductDetailsDTO();
-		ProductsDTO productsDTO = new ProductsDTO();
-		productsDTO = productDAO.selectByProductId(product_id);
-		List<ProductImgCarouselDTO> carouselLists = productImgsDAO.selectCarouselImgsByProductId(product_id);
-		List<ProductImgDetailDTO> detailLists = productImgsDAO.selectDetailImgsByProductId(product_id);
-		List<ColorsDTO> colorsLists = colorsDAO.selectByProductId(product_id);
-		List<SizesDTO> sizesLists = sizesDAO.selectByProductId(product_id);
-		for(ProductImgCarouselDTO carousel : carouselLists) {
-			carousel.setProduct_img_name("/webapp/image?path=" + carousel.getProduct_img_name());
-		}
-		for(ProductImgDetailDTO detail : detailLists) {
-			detail.setProduct_img_name("/webapp/image?path=" + detail.getProduct_img_name());
-		}
-		
-		productDetailsDTO.setProduct_id(productsDTO.getProduct_id());
-		productDetailsDTO.setProduct_name(productsDTO.getProduct_name());
-		productDetailsDTO.setProduct_price(productsDTO.getProduct_price());
-		productDetailsDTO.setProduct_content(productsDTO.getProduct_content());
-		productDetailsDTO.setProduct_subcontent(productsDTO.getProduct_subcontent());
-		String filePath = "/webapp/image?path="+ productsDTO.getProduct_image();
-		productDetailsDTO.setProduct_image(filePath);
-		productDetailsDTO.setProduct_imgs_carousel_list(carouselLists);
-		productDetailsDTO.setProduct_imgs_detail_list(detailLists);
-		productDetailsDTO.setProduct_colors_list(colorsLists);
-		productDetailsDTO.setProduct_sizes_list(sizesLists);
-		
-		return productDetailsDTO;
+		 return products;
 	}
 	
 	/**
 	 * 서비스 목적
-	 * - 모든 사용자들이 카테고리의 상품을 보기 위해 제공하는 서비스
-	 * - 컨트롤러에게 List<ProductsDTO> 를 전달해야 한다.
+	 * - 모든 사용자들이 상품의 상세 정보를 보기 위해 제공하는 서비스
 	 */
 	@Override
-	public List<ProductsDTO> showProductList(String category, String subcategory, String orderBy) {
-		// 1. - ProductsDAO에 selectProductListWithCategoryAndSubCategory(String category, String subcategory, String orderBy)를 실행하여 저장
-		// 2. 받은 List<ProductsDAO> 객체를 Controller로 넘긴다.
+	public ProductDetailsDTO showProductDetail(Long product_id) {
 		
+		ProductDetailsDTO productDetailsDTO = new ProductDetailsDTO();
+		ProductsDTO productsDTO = new ProductsDTO();
 		
+		productsDTO = productDAO.selectByProductId(product_id);
+		List<ProductImgCarouselDTO> carouselList = productImgsDAO.selectCarouselImgsByProductId(product_id);
+		List<ProductImgDetailDTO> detailList = productImgsDAO.selectDetailImgsByProductId(product_id);
+		List<ColorsDTO> colorsList = colorsDAO.selectByProductId(product_id);
+		List<SizesDTO> sizesList = sizesDAO.selectByProductId(product_id);
 		
-		return null;
+		//상품의 이미지 경로를 설정하기 위한 for문
+		for(ProductImgCarouselDTO carousel : carouselList) {
+			carousel.setProduct_img_name("/webapp/image?path=" + carousel.getProduct_img_name());
+		}
+		for(ProductImgDetailDTO detail : detailList) {
+			detail.setProduct_img_name("/webapp/image?path=" + detail.getProduct_img_name());
+		}
+		
+		String filePath = "/webapp/image?path="+ productsDTO.getProduct_image();
+		productDetailsDTO.setProduct_image(filePath);
+		productDetailsDTO.setProductDetailsInfo(productsDTO,carouselList,detailList,colorsList,sizesList);
+		
+		return productDetailsDTO;
 	}
-
+	
 	/**
 	 * 미구현 사항
 	 * 서비스 목적
--- src/main/webapp/WEB-INF/views/shop/all.jsp
+++ src/main/webapp/WEB-INF/views/shop/all.jsp
@@ -4,7 +4,6 @@
 <c:set var="contextPath" value="<%= request.getContextPath()%>"></c:set>  
 <c:set var="resources" value="${contextPath}/resources"></c:set>
 
-
 <!DOCTYPE html>
 <html lang="ko">
 <head>
@@ -37,53 +36,40 @@
     <section>
         <article>
             <div class="categories_article container-fluid">
-            
-  
-            	
-            <div class="categories_container">
-            	<!-- Left side -->
-            	<div class="categories_left_container">
-            		<h4 class="eng_h6 categories_inline">
-	            		${category}
-		            	<img src="${contextPath}/resources/svg/chevron-right.svg" width="24px" />
-            		</h4>
-            		<h4 class="eng_h3 categories_inline">
-            			${subcategory}
-            		</h4>
-            	
-            	</div>
-            	
-            	<!-- Right side -->
-            	<div class="categories_right_container">
-            	 <select name="item" id="item_select" class="categories_orders_select" >
-                        <option value="">Select</option>
-                        <option value="high">높은 가격순</option>
-                        <option value="low">낮은 가격순</option>
-                    </select>
-            	</div>
-            	
-            	
-            </div>	
-              <!--   <div class="categories_orders">
-                    <select name="item" id="item_select" class="categories_orders_select" >
-                        <option value="">Select</option>
-                        <option value="high">높은 가격순</option>
-                        <option value="low">낮은 가격순</option>
-                    </select>
-                </div> -->
-                
+	            <div class="categories_container">
+	            	<!-- Left side -->
+	            	<div class="categories_left_container">
+	            		<h4 class="eng_h6 categories_inline">
+		            		${category}
+			            	<img src="${contextPath}/resources/svg/chevron-right.svg" width="24px" />
+	            		</h4>
+	            		<h4 class="eng_h3 categories_inline">
+	            			${subcategory}
+	            		</h4>
+	            	
+	            	</div>
+	            	
+	            	<!-- Right side -->
+	            	<div class="categories_right_container">
+	            	 <select name="item" id="item_select" class="categories_orders_select">
+	                        <option value="">Select</option>
+	                        <option value="high">높은 가격순</option>
+	                        <option value="low">낮은 가격순</option>
+	                    </select>
+	            	</div>
+	            </div>	
                <c:forEach var="product_list" items="${lists}">
                	<div class="row">
                		<c:forEach var="product" items="${product_list}">
                			<div class="col-xs-12 col-md-6 col-lg-3" onclick="goProductDetailPage('${contextPath}','${product.product_id}')">
                         	<div class="categories_item">
                             	<img src="${product.product_image}" width="80%">
-                            <div class="categories_item_text">
-                                <h5 class="eng_h4">${product.product_name}</h5>
-                                <h5 class="eng_h4"><fmt:formatNumber value="${product.product_price}" pattern="#,###,###"/></h5>
-                            </div>
-                        </div>
-                    </div>
+	                            <div class="categories_item_text">
+	                                <h5 class="eng_h4">${product.product_name}</h5>
+	                                <h5 class="eng_h4"><fmt:formatNumber value="${product.product_price}" pattern="#,###,###"/></h5>
+	                            </div>
+                        	</div>
+                    	</div>
                		</c:forEach>
                		</div>
                </c:forEach>
